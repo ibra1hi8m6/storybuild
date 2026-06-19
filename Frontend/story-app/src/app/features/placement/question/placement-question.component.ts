3fc9f48d19413381e679d4173917f175781f18c9
basmahossam <basmahosamro@gmail.com>
2026-06-19 04:04:02 +0300

    update

--- Frontend/story-app/src/app/features/placement/question/placement-question.component.ts
+++ Frontend/story-app/src/app/features/placement/question/placement-question.component.ts
@@ -65,7 +65,6 @@ export class PlacementQuestionComponent implements OnInit, OnDestroy {
     this.isLoading.set(true);
     this.service.getPlacementQuestions().subscribe({
       next: qs => {
-        console.log('RAW BACKEND DATA:', qs);
         this.questions.set(this.normalizeQuestions(qs));
         this.isLoading.set(false);
         this.speakQuestion();
